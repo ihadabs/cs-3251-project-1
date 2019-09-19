d413b977354efef2f126bb4af2569c9c2df017d2
Hadi Albinsaad <ihadabs@gmail.com>
2019-09-19 02:39:09 -0400

    They can communicate now!

--- client.c
+++ client.c
@@ -1,79 +1,84 @@
 /*///////////////////////////////////////////////////////////
 *
 * FILE:		client.c
-* AUTHOR:	Your Name Here
+* AUTHOR:	Hadi Albinsaad
 * PROJECT:	CS 3251 Project 1 - Professor Jun Xu 
 * DESCRIPTION:	Network Client Code
 * CREDIT:	Adapted from Professor Traynor
 *
-*////////////////////////////////////////////////////////////
+*/
+///////////////////////////////////////////////////////////
 
 /* Included libraries */
 
-#include <stdio.h>		    /* for printf() and fprintf() */
-#include <sys/socket.h>		    /* for socket(), connect(), send(), and recv() */
-#include <arpa/inet.h>		    /* for sockaddr_in and inet_addr() */
+#include <stdio.h>      /* for printf() and fprintf() */
+#include <sys/socket.h> /* for socket(), connect(), send(), and recv() */
+#include <arpa/inet.h>  /* for sockaddr_in and inet_addr() */
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
 
 /* Constants */
-#define RCVBUFSIZE 512		    /* The receive buffer size */
-#define SNDBUFSIZE 512		    /* The send buffer size */
+#define RCVBUFSIZE 512 /* The receive buffer size */
+#define SNDBUFSIZE 512 /* The send buffer size */
 #define REPLYLEN 32
+
+#define PORT 9003
 
 /* The main function */
 int main(int argc, char *argv[])
 {
 
-    int clientSock;		    /* socket descriptor */
-    struct sockaddr_in serv_addr;   /* server address structure */
+    int client_socket;                 /* socket descriptor */
+    struct sockaddr_in server_address; /* server address structure */
 
-    char *accountName;		    /* Account Name  */
-    char *servIP;		    /* Server IP address  */
-    unsigned short servPort;	    /* Server Port number */
-    
+    char *account_name;         /* Account Name  */
+    char *server_ip;            /* Server IP address  */
+    unsigned short server_port; /* Server Port number */
 
-    char sndBuf[SNDBUFSIZE];	    /* Send Buffer */
-    char rcvBuf[RCVBUFSIZE];	    /* Receive Buffer */
-    
-    int balance;		    /* Account balance */
+    char send_buff[SNDBUFSIZE];    /* Send Buffer */
+    char receive_buff[RCVBUFSIZE]; /* Receive Buffer */
+
+    int balance; /* Account balance */
 
     /* Get the Account Name from the command line */
-    if (argc != 4) 
+    if (argc != 4)
     {
-	printf("Incorrect number of arguments. The correct format is:\n\taccountName serverIP serverPort");
-	exit(1);
+        printf("Incorrect number of arguments. The correct format is:\n\taccountName serverIP serverPort");
+        exit(1);
     }
-    accountName = argv[1];
-    memset(&sndBuf, 0, SNDBUFSIZE);
-    memset(&rcvBuf, 0, RCVBUFSIZE);
+    account_name = argv[1];
+    memset(&send_buff, 0, SNDBUFSIZE);
+    memset(&receive_buff, 0, RCVBUFSIZE);
 
     /* Get the addditional parameters from the command line */
     /*	    FILL IN	*/
 
     /* Create a new TCP socket*/
-    /*	    FILL IN	*/
-
+    client_socket = socket(AF_INET, SOCK_STREAM, 0);
 
     /* Construct the server address structure */
-    /*	    FILL IN	 */
-
+    server_address.sin_family = AF_INET;
+    server_address.sin_port = htons(PORT);
+    server_address.sin_addr.s_addr = INADDR_ANY;
 
     /* Establish connecction to the server */
-    /*	    FILL IN	 */
+    int connection_status = connect(client_socket, (struct sockaddr *)&server_address, sizeof(server_address));
+    if (connection_status == -1)
+    {
+        printf("There was an errror making a connection to the remote socket! \n");
+        exit(0);
+    }
 
-    
     /* Send the string to the server */
     /*	    FILL IN	 */
 
-
     /* Receive and print response from the server */
-    /*	    FILL IN	 */
+    recv(client_socket, &receive_buff, sizeof(receive_buff), 0);
+    printf("The server says: %s \n\n", receive_buff);
 
-    printf("%s\n", accountName);
+    printf("%s\n", account_name);
     printf("Balance is: %i\n", balance);
 
     return 0;
 }
-
--- server.c
+++ server.c
@@ -1,68 +1,99 @@
 /*///////////////////////////////////////////////////////////
 *
 * FILE:		server.c
-* AUTHOR:	Your Name Here
+* AUTHOR:	Hadi Albinsaad
 * PROJECT:	CS 3251 Project 1 - Professor Jun Xu
 * DESCRIPTION:	Network Server Code
 * CREDIT:	Adapted from Professor Traynor
 *
-*////////////////////////////////////////////////////////////
+*/
+///////////////////////////////////////////////////////////
 
 /*Included libraries*/
 
-#include <stdio.h>	  /* for printf() and fprintf() */
-#include <sys/socket.h>	  /* for socket(), connect(), send(), and recv() */
-#include <arpa/inet.h>	  /* for sockaddr_in and inet_addr() */
-#include <stdlib.h>	  /* supports all sorts of functionality */
-#include <unistd.h>	  /* for close() */
-#include <string.h>	  /* support any string ops */
+#include <stdio.h>  /* for printf() and fprintf() */
+#include <stdlib.h> /* supports all sorts of functionality */
 
-#define RCVBUFSIZE 512		/* The receive buffer size */
-#define SNDBUFSIZE 512		/* The send buffer size */
-#define BUFSIZE 40		/* Your name can be as many as 40 chars*/
+#include <sys/types.h>
+#include <sys/socket.h> /* for socket(), connect(), send(), and recv() */
+
+#include <netinet/in.h>
+#include <arpa/inet.h> /* for sockaddr_in and inet_addr() */
+
+#include <unistd.h> /* for close() */
+#include <string.h> /* support any string ops */
+
+#define RCVBUFSIZE 512 /* The receive buffer size */
+#define SNDBUFSIZE 512 /* The send buffer size */
+#define BUFSIZE 40     /* Your name can be as many as 40 chars*/
+#define PORT 9003
 
 /* The main function */
 int main(int argc, char *argv[])
 {
 
-    int serverSock;				/* Server Socket */
-    int clientSock;				/* Client Socket */
-    struct sockaddr_in changeServAddr;		/* Local address */
-    struct sockaddr_in changeClntAddr;		/* Client address */
-    unsigned short changeServPort;		/* Server port */
-    unsigned int clntLen;			/* Length of address data struct */
+    int server_socket;                 /* Server Socket */
+    int client_socket;                 /* Client Socket */
+    struct sockaddr_in server_address; /* Local address */
+    struct sockaddr_in client_address; /* Client address */
+    unsigned short server_port;        /* Server port */
+    unsigned int client_length;        /* Length of address data struct */
 
-    char nameBuf[BUFSIZE];			/* Buff to store account name from client */
-    int  balance;				/* Place to record account balance result */
+    char name_buff[BUFSIZE]; /* Buff to store account name from client */
+    int balance;             /* Place to record account balance result */
 
+    char server_message[256] = "You have reached the server!";
 
     /* Create new TCP Socket for incoming requests*/
-    /*	    FILL IN	*/
+    server_socket = socket(AF_INET, SOCK_STREAM, 0);
 
     /* Construct local address structure*/
-    /*	    FILL IN	*/
-    
+    server_address.sin_family = AF_INET;
+    server_address.sin_port = htons(PORT);
+    server_address.sin_addr.s_addr = INADDR_ANY;
+
     /* Bind to local address structure */
-    /*	    FILL IN	*/
+    if (bind(server_socket, (struct sockaddr *)&server_address, sizeof(server_address)) != 0)
+    {
+        printf("Socket bind failed...\n");
+        exit(0);
+    }
+    else
+        printf("Socket successfully binded..\n");
 
     /* Listen for incoming connections */
-    /*	    FILL IN	*/
+    if (listen(server_socket, 5) != 0)
+    {
+        printf("Listen failed...\n");
+        exit(0);
+    }
+    else
+        printf("Server listening..\n");
 
     /* Loop server forever*/
-    while(1)
+    while (1)
     {
+        /* Accept incoming connection */
+        // client_socket = accept(server_socket, (struct sockaddr *)&client_address, sizeof(client_address));
+        client_socket = accept(server_socket, NULL, NULL);
+        if (client_socket < 0)
+        {
+            printf("Server acccept failed...\n");
+            exit(0);
+        }
+        else
+            printf("Server acccept the client...\n");
 
-	/* Accept incoming connection */
-	/*	FILL IN	    */
+        /* Extract the account name from the packet, store in nameBuf */
+        read(client_socket, name_buff, sizeof(name_buff));
+        printf("Account name: %s\n", name_buff);
 
-	/* Extract the account name from the packet, store in nameBuf */
-	/* Look up account balance, store in balance */
-	/*	FILL IN	    */
+        /* Look up account balance, store in balance */
+        /*	FILL IN	    */
 
-	/* Return account balance to client */
-	/*	FILL IN	    */
+        /* Return account balance to client */
+        /*	FILL IN	    */
 
+        send(client_socket, server_message, sizeof(server_message), 0);
     }
-
 }
-
